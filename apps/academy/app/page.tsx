ca993d00b0417961983b1c1539c1603a95c338db
yesoryeseul <yesoryeseul07@gmail.com>
2024-06-22 01:55:13 +0900

    [MOBI-28-curriculum-schedule-desktop-publishing] Chore: 섹션 사용 부분 제거

--- apps/academy/app/page.tsx
+++ apps/academy/app/page.tsx
@@ -1,26 +1,14 @@
 import { Button } from '@repo/ui'
 
-import { CurriculumScheduleContents, CurriculumScheduleSection } from '@/widget/landing/ui'
-
 export default function Home() {
   return (
-    // <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-    <main>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Button
         className='border-[2px] border-black'
         size='small'
         intent='primary'>
         academy
       </Button>
-      <CurriculumScheduleSection
-        title={
-          <CurriculumScheduleSection.title className='text-center'>
-            모비 프론트 2기 <br />
-            커리큘럼 및 일정
-          </CurriculumScheduleSection.title>
-        }
-        info={<CurriculumScheduleContents />}
-      />
     </main>
   )
 }
