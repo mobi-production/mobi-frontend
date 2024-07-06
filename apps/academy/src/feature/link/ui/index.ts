02a72850d3cfe7b9cf205541b0681e687abd389e
yesoryeseul <yesoryeseul07@gmail.com>
2024-07-07 02:30:58 +0900

    [MOBI-33-landing-page-bottom-mobile-publishing] Fix: conflict merge

--- apps/academy/src/feature/link/ui/index.ts
+++ apps/academy/src/feature/link/ui/index.ts
@@ -1,3 +1,2 @@
-export { ApplyButton } from './ApplyButton'
 export { NavigationButton } from './NavigationButton'
 export { ResponsiveSupportedApplyButton } from './ResponsiveSupportedApplyButton'
--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -1,3 +1,3 @@
-export { cn } from './cn'
 export { blankOpener } from './blank-opener'
+export { cn } from './cn'
 export { formatDate } from './date-formatter'
